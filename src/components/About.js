3c40d2721370a5fcbc9d536853fe52523e04359d
Kevin Lang <kevinl20@yahoo.com>
2023-05-03 21:13:51 -0700

    added about me and contact.js

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section id="about">
+        <h2>About Me</h2>
+        <div className="information">
+          <p>Hi, my name is Kevin Lang. I'm working to be a Full Stack developer and I like 
+            turtle.
+             </p>
+             </div>
+             <div className="content">
+     I'm currently a student at the University of Washington. I'm currently
+     working on changing my career by being a full stack developer.</div>
+     <p>In my spare time, I like to play video games, work out and play basketball
+        or football. I'm also a dog dad to my 8 year old corgi name Picklez.
+     </p>
+           
+    </section>
+ );
+}
