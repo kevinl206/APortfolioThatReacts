988adeee6927177ce1ae62e9e92d6fc471e63b09
Kevin Lang <kevinl20@yahoo.com>
2023-05-03 21:27:24 -0700

    added footer code

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+  <footer id="contact">
+<h2> Contact me</h2>
+<p>kevinl206@yahoo.com</p>
+<p> AIM S/N: AzNsTuNna206</p>
+</footer>
+    )
+}
+
