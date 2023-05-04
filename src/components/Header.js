35f2cff44eb2d6949147d82490eb0779b358eb04
Kevin Lang <kevinl20@yahoo.com>
2023-05-03 22:18:00 -0700

    npm react-dom and added profile picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import noPicture from "./assets/noPicture.png"
 
 export default function Header() {
     return (
@@ -7,6 +8,7 @@ export default function Header() {
           <h4>Hello I'm</h4>
           <h1>Kevin Lang</h1>
           <h5 className="text-light">Fullstack Developer</h5>
+          <img src={noPicture} alt="nopicture"></img>
 
         </div>
       </header>
